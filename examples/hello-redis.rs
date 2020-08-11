4c0060c9b332606d338d66fc6a2bbf567ba0a448
Nico Graglia <nicograglia@gmail.com>
2020-08-11 16:17:39 -0300

    client multi task impl

--- examples/hello-redis.rs
+++ examples/hello-redis.rs
@@ -1,17 +1,89 @@
-use mini_redis::{client, Result};
+use mini_redis::{client};
+use tokio::sync::mpsc;
+use tokio::sync::oneshot;
+
+use bytes::Bytes;
+
+/// Multiple different commands are multiplexed over a single channel.
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+        resp: Responder<Option<Bytes>>,
+    },
+    Set {
+        key: String,
+        val: Vec<u8>,
+        resp: Responder<()>,
+    },
+}
+
+/// Provided by the requester and used by the manager task to send the command
+/// response back to the requester.
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 
 #[tokio::main]
-pub async fn main() -> Result<()> {
-    // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+async fn main() {
+     // Create a new channel with a capacity of at most 32.
+     let (mut tx, mut rx) = mpsc::channel(32);
 
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
+     let mut tx2 = tx.clone();
 
-    // Get key "hello"
-    let result = client.get("hello").await?;
+     // Spawn two tasks, one gets a key, the other sets a key
+     let t1 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Get {
+            key: "hello".to_string(),
+            resp: resp_tx,
+        };
+    
+        // Send the GET request
+        tx.send(cmd).await.unwrap();
+    
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res);
+    });
 
-    println!("got value from the server; result={:?}", result);
+    let t2 = tokio::spawn(async move {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: b"bar".to_vec(),
+            resp: resp_tx,
+        };
+    
+        // Send the SET request
+        tx2.send(cmd).await.unwrap();
+    
+        // Await the response
+        let res = resp_rx.await;
+        println!("GOT = {:?}", res)
+    });
 
-    Ok(())
+     // The `move` keyword is used to **move** ownership of `rx` into the task.
+    let manager = tokio::spawn(async move {
+        // Establish a connection to the server
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Command::Get { key, resp } => {
+                    let res = client.get(&key).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+                Command::Set { key, val, resp } => {
+                    let res = client.set(&key, val.into()).await;
+                    // Ignore errors
+                    let _ = resp.send(res);
+                }
+            }
+        }
+    });
+
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 }
